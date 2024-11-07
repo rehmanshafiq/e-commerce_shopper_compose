e16bcbf9f97ee5d0bbf69bf74ba0121e86721729
Shafiq <shafiq.rehman@nextgeni.com>
2024-11-07 10:35:45 +0500

    Model changes.

--- data/src/main/java/com/shafiq/data/model/DataProductModel.kt
+++ data/src/main/java/com/shafiq/data/model/DataProductModel.kt
@@ -8,7 +8,7 @@ class DataProductModel (
     val id: Int,
     val title: String,
     val price: Double,
-//    val categoryId: Int,
+    val category: String,
     val description: String,
     val image: String
 ) {
@@ -16,7 +16,7 @@ class DataProductModel (
         id = id,
         title = title,
         price = price,
-//        category_id = category_id,
+        category = category,
         description = description,
         image = image
     )
--- domain/src/main/java/com/shafiq/domain/model/Product.kt
+++ domain/src/main/java/com/shafiq/domain/model/Product.kt
@@ -4,7 +4,7 @@ data class Product(
     val id: Int,
     val title: String,
     val price: Double,
-//    val category_id: Int?,
+    val category: String,
     val description: String,
     val image: String
 ) {
