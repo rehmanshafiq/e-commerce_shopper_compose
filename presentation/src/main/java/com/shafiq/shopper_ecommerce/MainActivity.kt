135e89d45a8c72c0c5f55604dc62a3fc2ad55d05
Shafiq <shafiq.rehman@nextgeni.com>
2024-11-11 15:15:56 +0500

    Implemented Bottom navigation bar along with their routing.

--- presentation/src/main/java/com/shafiq/shopper_ecommerce/MainActivity.kt
+++ presentation/src/main/java/com/shafiq/shopper_ecommerce/MainActivity.kt
@@ -3,16 +3,25 @@ package com.shafiq.shopper_ecommerce
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.shafiq.shopper_ecommerce.feature.home.HomeScreen
 import com.shafiq.shopper_ecommerce.ui.theme.Shopper_ecommerceTheme
@@ -23,12 +32,85 @@ class MainActivity : ComponentActivity() {
         setContent {
             Shopper_ecommerceTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "home") {
-                    composable("home") {
-                        HomeScreen(navController)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        BottomNavigationBar(navController = navController)
+                    }
+                ) {
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(it)
+                    ) {
+                        NavHost(navController = navController, startDestination = "home") {
+                            composable("home") {
+                                HomeScreen(navController)
+                            }
+                            composable("cart") {
+                                HomeScreen(navController)
+                            }
+                            composable("profile") {
+                                HomeScreen(navController)
+                            }
+                        }
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    BottomAppBar {
+        // current route
+        val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+        val items = listOf(
+            BottomNavItems.Home,
+            BottomNavItems.Cart,
+            BottomNavItems.Profile
+        )
+        items.forEach { item ->
+            NavigationBarItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { startRoute ->
+                            popUpTo(startRoute) {
+                                saveState = true
+                            }
+                        }
+                    }
+                },
+                label = {
+                    Text(text = item.title)
+                },
+                icon = {
+                    Image(
+                        painter = painterResource(id = item.icon),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(
+                            if (currentRoute == item.route)
+                                MaterialTheme.colorScheme.primary
+                            else
+                                Color.Gray
+                        )
+                    )
+                },
+                colors = NavigationBarItemDefaults.colors().copy(
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedTextColor = Color.Gray,
+                    unselectedIconColor = Color.Gray
+                )
+            )
+        }
+    }
+}
+
+sealed class BottomNavItems(val route: String, val title: String, val icon: Int) {
+    data object Home : BottomNavItems("home", "Home", icon = R.drawable.ic_home)
+    data object Cart : BottomNavItems("cart", "Cart", icon = R.drawable.ic_cart)
+    data object Profile : BottomNavItems("profile", "Profile", icon = R.drawable.ic_profile_nav)
 }
