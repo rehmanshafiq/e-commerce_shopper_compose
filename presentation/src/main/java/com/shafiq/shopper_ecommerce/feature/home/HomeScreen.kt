06d3978c25001f7b38a6e5af4a63e23e5a621905
Shafiq <shafiq.rehman@nextgeni.com>
2024-11-11 10:47:37 +0500

    Search bar implemented in home screen.

--- presentation/src/main/java/com/shafiq/shopper_ecommerce/feature/home/HomeScreen.kt
+++ presentation/src/main/java/com/shafiq/shopper_ecommerce/feature/home/HomeScreen.kt
@@ -1,16 +1,21 @@
 package com.shafiq.shopper_ecommerce.feature.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,17 +24,23 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.shafiq.domain.model.Product
+import com.shafiq.shopper_ecommerce.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -63,8 +74,57 @@ fun HomeScreen(
 }
 
 @Composable
+fun ProfileHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_profile),
+                contentDescription = null,
+                modifier = Modifier.size(48.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = "Hello!",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text = "John Doe",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+        Image(
+            painter = painterResource(id = R.drawable.ic_bell),
+            contentDescription = null,
+            modifier = Modifier
+                .size(48.dp)
+                .align(Alignment.CenterEnd)
+                .clip(CircleShape)
+                .background(Color.LightGray.copy(alpha = 0.3f))
+                .padding(8.dp),
+            contentScale = ContentScale.Inside
+        )
+    }
+}
+
+@Composable
 fun HomeContent(featured: List<Product>, popularProducts:List<Product>) {
     LazyColumn {
+        item {
+            ProfileHeader()
+            Spacer(modifier = Modifier.size(16.dp))
+            SearchBar(value = "", onValueChange = {})
+            Spacer(modifier = Modifier.size(16.dp))
+        }
         item {
             if (featured.isNotEmpty()) {
                 HomeProductRow(products = featured, title = "Featured")
@@ -78,6 +138,44 @@ fun HomeContent(featured: List<Product>, popularProducts:List<Product>) {
 }
 
 @Composable
+fun SearchBar(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        shape = RoundedCornerShape(32.dp),
+        leadingIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.ic_search), 
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            focusedPlaceholderColor = Color.Transparent,
+            unfocusedLabelColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent,
+            focusedContainerColor = Color.LightGray.copy(alpha = 0.3f),
+            unfocusedContainerColor = Color.LightGray.copy(alpha = 0.3f)
+        ),
+        placeholder = {
+            Text(
+                text = "Search for products",
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+    )
+}
+
+@Composable
 fun HomeProductRow(products: List<Product>, title:String) {
     Column {
         Box(modifier = Modifier
@@ -87,7 +185,8 @@ fun HomeProductRow(products: List<Product>, title:String) {
             Text(
                 text = title,
                 style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.align(Alignment.CenterStart)
+                modifier = Modifier.align(Alignment.CenterStart),
+                fontWeight = FontWeight.SemiBold
             )
             Text(
                 text = "View all",
@@ -110,7 +209,7 @@ fun ProductItem(product: Product) {
     Card(
         modifier = Modifier
             .padding(horizontal = 8.dp)
-            .size(width = 126.dp, height = 144.dp),
+            .size(width = 126.dp, height = 150.dp),
         shape = RoundedCornerShape(16.dp),
         colors = CardDefaults.cardColors(contentColor = Color.LightGray.copy(alpha = 0.3f))
     ) {
@@ -131,6 +230,7 @@ fun ProductItem(product: Product) {
                 modifier = Modifier.padding(horizontal = 8.dp),
                 fontWeight = FontWeight.SemiBold,
                 maxLines = 1,
+                color = Color.Black,
                 overflow = TextOverflow.Ellipsis
             )
             Text(
